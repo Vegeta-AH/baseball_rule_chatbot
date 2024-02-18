72613a93f31d0bb7cc813b1a79af00921539605d
Vegeta-AH <157863101+Vegeta-AH@users.noreply.github.com>
2024-02-18 21:47:21 +0900

    11st commit

--- app.py
+++ app.py
@@ -2,12 +2,21 @@
 import streamlit as st
 import requests
 
-st.title("野球AIチャットボット")
+st.title("野球ルールAIチャットボット")
+
+# 想定される質問の提示
+st.sidebar.header("質問例:")
+st.sidebar.markdown("""
+- 投手板から本塁の距離は？
+- 野球でピッチャーの役割を教えて？
+- プロ野球では、金属製バットは使用できる？
+- 試合中、ベンチに入れる人は？
+- ２人の走者が同一のフェアボールに触れた場合に2人ともアウトになるのか？
+""")
 
 # Streamlit UIの構築
 user_input = st.text_input("Enter a question:")
 button = st.button("Get Answer")
-
 
 # ボタンがクリックされたときの処理
 if button:
@@ -15,9 +24,19 @@ if button:
     #response = requests.post("http://localhost:8000/answer", json={"text": user_input})
     response = requests.post("https://baseball-rule-chatbot.onrender.com/answer", json={"text": user_input})
     answer = response.json().get("answer", "No answer available.")
+    docs = response.json().get("docs", [])
 
     #バーンを表示
     st.balloons()
     
     # 応答を表示
-    st.write(f"Answer: {answer}")
+    st.subheader("回答:")
+    st.write(f"Answer: {answer['result']}")
+
+    # 関連文書を表示、もし存在する場合
+    if docs:
+        st.subheader("以下の関連文書を参考に回答しています：")
+        for doc in docs:
+            st.markdown(f"**文書{doc['doc_num']}**: {doc['content']}")
+    else:
+        st.write("関連する文書が見つかりませんでした。")
--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 #main.py
 
 #windows
-#import sqlite3
+import sqlite3
 #Linux systemの場合はrequirements.txt に pysqlite3-binary を指定
-import pysqlite3.dbapi2 as sqlite3
+#import pysqlite3.dbapi2 as sqlite3
 
 import sys
 sys.modules['sqlite3'] = sqlite3
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-
-import os
 import openai
 
 #必要なライブラリをインポート
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
-import chromadb
-from chromadb.config import Settings
 
 # OpenAI を使うためのインポート
 from langchain_openai import ChatOpenAI
 
 # 質問と回答の取得に使用するチェーンをインポート
 from langchain.chains import RetrievalQA
+
+# 本書の説明に合わせて少し変更しています
+from langchain.prompts import PromptTemplate
 
 app = FastAPI()
 class Question(BaseModel):
@@ -34,12 +33,9 @@ class Question(BaseModel):
 #key = 'key'
 #os.environ['OPENAI_API_KEY'] = key
 
-
+import os
 # openai.api_keyにOpenAIのAPIキーを入れる
 openai.api_key = os.environ['OPENAI_API_KEY']
-
-persist_directory = 'persist_directory'
-client = chromadb.PersistentClient(path=persist_directory)
 
 embeddings= OpenAIEmbeddings()
 
@@ -47,21 +43,43 @@ embeddings= OpenAIEmbeddings()
 db2 = Chroma(
     collection_name="langchain_store",
     embedding_function=embeddings,
-    client=client,
+    persist_directory = 'persist_directory',
 )
 
 # データベースからretriever作成
-retriever = db2.as_retriever(search_kwargs={"k": 3}) # Topkもここの引数で指定できる
+#retriever = db2.as_retriever(search_kwargs={"k": 3}) # Topkもここの引数で指定できる
+retriever = db2.as_retriever(search_type="similarity_score_threshold",search_kwargs={"score_threshold": .8}) #類似度の閾値を設定する。
 
 # LLM ラッパーの初期化
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, max_tokens=500)
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, max_tokens=1000)
+
+#promptを追加
+prompt_template = """Use the following pieces of context to answer the question at the end.
+If you don't get the answer, just say that you don't know.
+Don't try to make up an answer. And Answer in Japanese.
+
+ {context}
+
+Question: {question}:"""
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
 
 # チェーンを作り、それを使って質問に答える
-qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever,chain_type_kwargs={"prompt": PROMPT},
+        # 参考にした文書もレスポンスに含める
+        return_source_documents=True,
+        verbose=True
+        )
 
 @app.post("/answer")
 def get_answer(question: Question):
     # ここでモデルに質問を渡し、応答を取得するロジックを追加
     query = question.text
-    answer = qa.run(query)
-    return {"answer": answer}
+    answer = qa.invoke(query)
+    docs = retriever.get_relevant_documents(query)
+    # docsの内容を整形してレスポンスに含める
+    formatted_docs = [{"doc_num": num + 1, "content": doc.page_content} for num, doc in enumerate(docs)]
+
+    return {"answer": answer, "docs": formatted_docs}
+    #return {"docs": formatted_docs}
